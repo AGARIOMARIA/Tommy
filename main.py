34d6f80dc795024962997d5def9f5afa5e214bce
AGARIOMARIA <missmariacoyle@gmail.com>
2025-05-20 13:02:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-print('hello! I love you so much!')
+print("hello! I love you so much!")
 mylovesname = input("please enter your name:")
 
 #HI LOVE
-if mylovesname = "Tommy" or "tommy" or "Tommy Russoniello"
+if mylovesname == "Tommy" or mylovesname == "tommy" or mylovesname == "Tommy Russoniello"
 print("I LOVE YOU!!!!")
 else: 
 print("get out of here! I want Tommy")
